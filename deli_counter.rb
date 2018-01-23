cdea8a390407e4594343add09b85549962852d93
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-23 21:58:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0 
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
+  end
 end 
