14ad947a528db1e6be0ba4d18b5a0ef4ae3a51f9
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-23 21:53:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,10 @@ end
 
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
+  if katz_deli.length == 0
+    puts "Welcome #{name}. You are number " + katz_deli.length + 1 + " in line."
+  end 
+
+
+  #katz_deli.push(name)
 end
