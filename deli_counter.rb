57cc33f0019f9179b882af76e01fabba13c20dec
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-23 21:32:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,11 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-  
-
+  x = 1
+    while x < katz_deli.length
+    katz_deli.push(" " + x + ". " + katz_deli[x])
+    x += 1
+  end
+  katz_deli
 end
 end
